27320488b8cf164c6a847bdebf61522319348ac1
anishapant21 <anishapant1@gmail.com>
2024-08-13 15:40:10 -0400

    API integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chats" element={<ChatPage />} />
       </Routes>
     </>
   );
